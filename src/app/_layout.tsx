fb6a2542bbf177565e662dc1939456accedaa6f4
.type <seuchicolive@gmail.com>
2026-03-29 09:58:13 -0300

    feat: initialize project structure with Expo Router, custom theme constants, and updated dependencies

---
+++ src/app/_layout.tsx
@@ -0,0 +1,36 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({});
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
---
+++ src/types/expo-router.d.ts
@@ -0,0 +1,12 @@
+declare module 'expo-router' {
+  export const Stack: any;
+  export const Tabs: any;
+  export const useRouter: any;
+  export const useLocalSearchParams: any;
+  export const useSegments: any;
+  export const usePathname: any;
+  export const Redirect: any;
+  export const Link: any;
+  export const Slot: any;
+  export const ErrorBoundary: any;
+}
